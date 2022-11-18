62f6fc13e3c058ec5713a54f6cb586687b2c55ab
qgxx <qgxx2825313915@outlook.com>
2022-11-18 20:02:05 +0800

    soil

--- support.py
+++ support.py
@@ -6,10 +6,22 @@ def import_folder(path):
     surface_list = []
 
     # folder_name, sub_folder, img_list
-    for _, __, img_lists in walk(path):
-        for img in img_lists:
-            full_path = path + '/' + img
+    for _, __, img_files in walk(path):
+        for image in img_files:
+            full_path = path + '/' + image
             image_surface = pygame.image.load(full_path).convert_alpha()
             surface_list.append(image_surface)
 
     return surface_list
+
+
+def import_folder_dict(path):
+    surface_dict = {}
+
+    for _, __, ima_files in walk(path):
+        for image in ima_files:
+            full_path = path + '/' + image
+            image_surf = pygame.image.load(full_path).convert_alpha()
+            surface_dict[image.split('.')[0]] = image_surf
+
+    return surface_dict
